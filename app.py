9c1410eb356532afb3976214acf859992ae88f98
Charlie Quick <cquickreal00@gmail.com>
2023-06-20 09:30:09 -0400

    Changed formatting
    Changed the formatting to use the black formatter.

--- app.py
+++ app.py
@@ -6,40 +6,51 @@ import os
 
 # --- Location --- #
 here = os.path.dirname(os.path.abspath(__file__))
-icon = 'images/logo-512x512.ico'
+icon = "images/logo-512x512.ico"
 icon_fullpath = os.path.join(here, icon)
 
 # --- Window Settings --- #
 
 # Set theme
-sg.theme('DarkPurple')
+sg.theme("DarkPurple")
 
 # Create layout for the window
-layout = [[sg.Text('Enter number of desired moves, select your puzzle from the dropdown (leave blank for defaults), then hit generate!', pad=6)],
-          [sg.InputText(default_text='', size=(10, 1), pad=6, justification='center'), sg.Combo(values=['2x2', '3x3'], default_value='3x3', pad=6), sg.Button(
-              'Generate Scramble!', pad=6), sg.Button('Exit', pad=6)],
-          [sg.Text(key='-SCRAMBLE-', pad=6)]]
+layout = [
+    [
+        sg.Text(
+            "Enter number of desired moves, select your puzzle from the dropdown (leave blank for defaults), then hit generate!",
+            pad=6,
+        )
+    ],
+    [
+        sg.InputText(default_text="", size=(10, 1), pad=6, justification="center"),
+        sg.Combo(values=["2x2", "3x3"], default_value="3x3", pad=6),
+        sg.Button("Generate Scramble!", pad=6),
+        sg.Button("Exit", pad=6),
+    ],
+    [sg.Text(key="-SCRAMBLE-", pad=6)],
+]
 
 # Create the window
-window = sg.Window('Scramble Generator', layout, icon=icon_fullpath,
-                   element_justification='center')
+window = sg.Window(
+    "Scramble Generator", layout, icon=icon_fullpath, element_justification="center"
+)
 
 # Event loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
-    if event == 'Generate Scramble!' and values[0] != '':
-        if values[1] == '2x2':
-            window['-SCRAMBLE-'].update(functions.two_by_two(int(values[0])))
-        if values[1] == '3x3':
-            window['-SCRAMBLE-'].update(
-                functions.three_by_three(int(values[0])))
-    if event == 'Generate Scramble!' and values[0] == '':
-        if values[1] == '2x2':
-            window['-SCRAMBLE-'].update(functions.two_by_two())
-        if values[1] == '3x3':
-            window['-SCRAMBLE-'].update(functions.three_by_three())
+    if event == "Generate Scramble!" and values[0] != "":
+        if values[1] == "2x2":
+            window["-SCRAMBLE-"].update(functions.two_by_two(int(values[0])))
+        if values[1] == "3x3":
+            window["-SCRAMBLE-"].update(functions.three_by_three(int(values[0])))
+    if event == "Generate Scramble!" and values[0] == "":
+        if values[1] == "2x2":
+            window["-SCRAMBLE-"].update(functions.two_by_two())
+        if values[1] == "3x3":
+            window["-SCRAMBLE-"].update(functions.three_by_three())
         # window['-SCRAMBLE-'].update('The number of moves cannot be empty!')
-    if event == sg.WIN_CLOSED or event == 'Exit':
+    if event == sg.WIN_CLOSED or event == "Exit":
         break
 
 window.close()
--- functions.py
+++ functions.py
@@ -5,6 +5,7 @@ import random
 
 # --- Functions --- #
 
+
 def two_by_two(num_moves=9):
     # Function for generating random moves for standard 2x2 cube puzzles
     # Possible moves are: Right (R), Right Inverted (R'), Left (L), Left Inverted (L'), Up (U),
@@ -18,28 +19,33 @@ def two_by_two(num_moves=9):
 
     while counter < num_moves:
         if counter == 0:
-            scramble_order.append(random.choice(
-                top_moves+right_moves+front_moves))
+            scramble_order.append(random.choice(top_moves + right_moves + front_moves))
         else:
             match scramble_order[-1]:
                 case "U ":
-                    scramble_order.append(random.choice(
-                        ["U "]+right_moves+front_moves))
+                    scramble_order.append(
+                        random.choice(["U "] + right_moves + front_moves)
+                    )
                 case "U' ":
-                    scramble_order.append(random.choice(
-                        ["U' "]+right_moves+front_moves))
+                    scramble_order.append(
+                        random.choice(["U' "] + right_moves + front_moves)
+                    )
                 case "R ":
-                    scramble_order.append(random.choice(
-                        top_moves+["R "]+front_moves))
+                    scramble_order.append(
+                        random.choice(top_moves + ["R "] + front_moves)
+                    )
                 case "R' ":
-                    scramble_order.append(random.choice(
-                        top_moves+["R' "]+front_moves))
+                    scramble_order.append(
+                        random.choice(top_moves + ["R' "] + front_moves)
+                    )
                 case "F ":
-                    scramble_order.append(random.choice(
-                        top_moves+right_moves+["F "]))
+                    scramble_order.append(
+                        random.choice(top_moves + right_moves + ["F "])
+                    )
                 case "F' ":
-                    scramble_order.append(random.choice(
-                        top_moves+right_moves+["F' "]))
+                    scramble_order.append(
+                        random.choice(top_moves + right_moves + ["F' "])
+                    )
         counter += 1
     scramble_order = "".join(scramble_order)
     return scramble_order
@@ -61,46 +67,114 @@ def three_by_three(num_moves=20):
 
     while counter < num_moves:
         if counter == 0:
-            scramble_order.append(random.choice(
-                top_moves+bottom_moves+left_moves+right_moves+front_moves+back_moves))
+            scramble_order.append(
+                random.choice(
+                    top_moves
+                    + bottom_moves
+                    + left_moves
+                    + right_moves
+                    + front_moves
+                    + back_moves
+                )
+            )
         else:
             match scramble_order[-1]:
                 case "U ":
-                    scramble_order.append(random.choice(
-                        ["U "]+left_moves+right_moves+front_moves+back_moves))
+                    scramble_order.append(
+                        random.choice(
+                            ["U "] + left_moves + right_moves + front_moves + back_moves
+                        )
+                    )
                 case "U' ":
-                    scramble_order.append(random.choice(
-                        ["U' "]+left_moves+right_moves+front_moves+back_moves))
+                    scramble_order.append(
+                        random.choice(
+                            ["U' "]
+                            + left_moves
+                            + right_moves
+                            + front_moves
+                            + back_moves
+                        )
+                    )
                 case "D ":
-                    scramble_order.append(random.choice(
-                        ["D "]+left_moves+right_moves+front_moves+back_moves))
+                    scramble_order.append(
+                        random.choice(
+                            ["D "] + left_moves + right_moves + front_moves + back_moves
+                        )
+                    )
                 case "D' ":
-                    scramble_order.append(random.choice(
-                        ["D' "]+left_moves+right_moves+front_moves+back_moves))
+                    scramble_order.append(
+                        random.choice(
+                            ["D' "]
+                            + left_moves
+                            + right_moves
+                            + front_moves
+                            + back_moves
+                        )
+                    )
                 case "L ":
-                    scramble_order.append(random.choice(
-                        top_moves+bottom_moves+["L "]+front_moves+back_moves))
+                    scramble_order.append(
+                        random.choice(
+                            top_moves + bottom_moves + ["L "] + front_moves + back_moves
+                        )
+                    )
                 case "L' ":
-                    scramble_order.append(random.choice(
-                        top_moves+bottom_moves+["L' "]+front_moves+back_moves))
+                    scramble_order.append(
+                        random.choice(
+                            top_moves
+                            + bottom_moves
+                            + ["L' "]
+                            + front_moves
+                            + back_moves
+                        )
+                    )
                 case "R ":
-                    scramble_order.append(random.choice(
-                        top_moves+bottom_moves+["R "]+front_moves+back_moves))
+                    scramble_order.append(
+                        random.choice(
+                            top_moves + bottom_moves + ["R "] + front_moves + back_moves
+                        )
+                    )
                 case "R' ":
-                    scramble_order.append(random.choice(
-                        top_moves+bottom_moves+["R' "]+front_moves+back_moves))
+                    scramble_order.append(
+                        random.choice(
+                            top_moves
+                            + bottom_moves
+                            + ["R' "]
+                            + front_moves
+                            + back_moves
+                        )
+                    )
                 case "F ":
-                    scramble_order.append(random.choice(
-                        top_moves+bottom_moves+left_moves+right_moves+["F "]))
+                    scramble_order.append(
+                        random.choice(
+                            top_moves + bottom_moves + left_moves + right_moves + ["F "]
+                        )
+                    )
                 case "F' ":
-                    scramble_order.append(random.choice(
-                        top_moves+bottom_moves+left_moves+right_moves+["F' "]))
+                    scramble_order.append(
+                        random.choice(
+                            top_moves
+                            + bottom_moves
+                            + left_moves
+                            + right_moves
+                            + ["F' "]
+                        )
+                    )
                 case "B ":
-                    scramble_order.append(random.choice(
-                        top_moves+bottom_moves+left_moves+right_moves+["B "]))
+                    scramble_order.append(
+                        random.choice(
+                            top_moves + bottom_moves + left_moves + right_moves + ["B "]
+                        )
+                    )
                 case "B' ":
-                    scramble_order.append(random.choice(
-                        top_moves+bottom_moves+left_moves+right_moves+["B' "]))
+                    scramble_order.append(
+                        random.choice(
+                            top_moves
+                            + bottom_moves
+                            + left_moves
+                            + right_moves
+                            + ["B' "]
+                        )
+                    )
         counter += 1
     scramble_order = "".join(scramble_order)
     return scramble_order
@@ -108,13 +182,17 @@ def three_by_three(num_moves=20):
 
 # --- Main --- #
 
+
 def main():
     # Main block used for testing and running directly (not as imported lirbary)
 
     counter = 0
     try:
-        num_moves = int(input(
-            "Enter the number of moves you'd like the scrambler to generate (leave blank for defaults): "))
+        num_moves = int(
+            input(
+                "Enter the number of moves you'd like the scrambler to generate (leave blank for defaults): "
+            )
+        )
     except ValueError:
         num_moves = 9
 
