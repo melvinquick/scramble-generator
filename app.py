4231037bea194bb2a81711a0dfb540ccd22a6bd9
Charlie Quick <cquickreal00@gmail.com>
2024-03-25 10:18:30 -0400

    Update app.py
    Complete re-write of the GUI from PySimpleGui to PyQt6. This is the initial starting point for further development. Will be adding to this GUI from now on.

--- app.py
+++ app.py
@@ -1,56 +1,44 @@
 # --- Libraries --- #
 
-import PySimpleGUI as sg
+import sys
 import functions
-import os
 
-# --- Location --- #
-here = os.path.dirname(os.path.abspath(__file__))
-icon = "images/logo-512x512.ico"
-icon_fullpath = os.path.join(here, icon)
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QLabel, QVBoxLayout
 
-# --- Window Settings --- #
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
-# Set theme
-sg.theme("DarkPurple")
+        # Setup window title
+        self.setWindowTitle("Scramble Generator")
 
-# Create layout for the window
-layout = [
-    [
-        sg.Text(
-            "Enter number of desired moves, select your puzzle from the dropdown (leave blank for defaults), then hit generate!",
-            pad=6,
-        )
-    ],
-    [
-        sg.InputText(default_text="", size=(10, 1), pad=6, justification="center"),
-        sg.Combo(values=["2x2", "3x3"], default_value="3x3", pad=6),
-        sg.Button("Generate Scramble!", pad=6),
-        sg.Button("Exit", pad=6),
-    ],
-    [sg.Text(key="-SCRAMBLE-", pad=6)],
-]
+        # Setup page layout
+        page = QVBoxLayout()
+        button = QPushButton("Generate Scramble")
+        self.scramble = QLabel()
+        self.scramble.setAlignment(Qt.AlignmentFlag.AlignVCenter | Qt.AlignmentFlag.AlignHCenter)
 
-# Create the window
-window = sg.Window(
-    "Scramble Generator", layout, icon=icon_fullpath, element_justification="center"
-)
+        page.addWidget(button)
+        page.addWidget(self.scramble)
 
-# Event loop to process "events" and get the "values" of the inputs
-while True:
-    event, values = window.read()
-    if event == "Generate Scramble!" and values[0] != "":
-        if values[1] == "2x2":
-            window["-SCRAMBLE-"].update(functions.two_by_two(int(values[0])))
-        if values[1] == "3x3":
-            window["-SCRAMBLE-"].update(functions.three_by_three(int(values[0])))
-    if event == "Generate Scramble!" and values[0] == "":
-        if values[1] == "2x2":
-            window["-SCRAMBLE-"].update(functions.two_by_two())
-        if values[1] == "3x3":
-            window["-SCRAMBLE-"].update(functions.three_by_three())
-        # window['-SCRAMBLE-'].update('The number of moves cannot be empty!')
-    if event == sg.WIN_CLOSED or event == "Exit":
-        break
+        # Connect button to function to get scramble moves
+        button.pressed.connect(self.get_moves)
 
-window.close()
+        # Create dummy widget to hold layout and then set the central widget to the dummy widget
+        gui = QWidget()
+        gui.setLayout(page)
+
+        self.setCentralWidget(gui)
+
+    def get_moves(self):
+        self.scramble.setText(functions.moves())
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
+
+if __name__ == "__main__":
+    main()
