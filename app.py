bd11dc4671540c5e1ff753b47193c3603388ab95
Charlie Quick <cquickreal00@gmail.com>
2024-03-25 15:43:22 -0400

    Update app.py
    Added in ability to select puzzle types and number of moves. You can now select from 2x2 and 3x3 puzzles and a range of moves from 9 to 130.
    
    Added in Window Icon to window.

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 # --- Libraries --- #
 
 import sys
+import os
+import inspect
 import functions
 
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QLabel, QVBoxLayout
+from PyQt6.QtGui import QIcon
+from PyQt6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QWidget,
+    QPushButton,
+    QLabel,
+    QVBoxLayout,
+    QComboBox,
+    QHBoxLayout,
+    QSpinBox,
+)
+
+# dir = os.path.realpath(__file__)
+os.chdir(os.path.realpath(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))))
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -13,13 +29,37 @@ class MainWindow(QMainWindow):
         # Setup window title
         self.setWindowTitle("Scramble Generator")
 
+        # Set minimum window size
+        self.setMinimumSize(360, 100)
+
+        # Set window icon to logo
+        self.setWindowIcon(QIcon("images/logo-512x512.ico"))
+
         # Setup page layout
         page = QVBoxLayout()
-        button = QPushButton("Generate Scramble")
-        self.scramble = QLabel()
-        self.scramble.setAlignment(Qt.AlignmentFlag.AlignVCenter | Qt.AlignmentFlag.AlignHCenter)
 
-        page.addWidget(button)
+        inputs = QHBoxLayout()
+
+        button = QPushButton("Generate Scramble")
+
+        self.puzzle_type = QComboBox()
+        self.puzzle_type.addItems(["2x2", "3x3"])
+        self.puzzle_type.setCurrentIndex(1)
+
+        self.num_moves = QSpinBox()
+        self.num_moves.setRange(9, 130)
+        self.num_moves.lineEdit().setReadOnly(True)
+
+        self.scramble = QLabel()
+        self.scramble.setAlignment(
+            Qt.AlignmentFlag.AlignVCenter | Qt.AlignmentFlag.AlignHCenter
+        )
+
+        inputs.addWidget(button)
+        inputs.addWidget(self.puzzle_type)
+        inputs.addWidget(self.num_moves)
+
+        page.addLayout(inputs)
         page.addWidget(self.scramble)
 
         # Connect button to function to get scramble moves
@@ -32,13 +72,17 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(gui)
 
     def get_moves(self):
-        self.scramble.setText(functions.moves())
+        self.scramble.setText(
+            functions.moves(self.num_moves.value(), self.puzzle_type.currentText())
+        )
+
 
 def main():
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     app.exec()
+
 
 if __name__ == "__main__":
     main()
