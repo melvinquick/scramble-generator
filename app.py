11cd4f003ed6ddc0af753e582d14a86656f0caca
Charlie Quick <melvinquick@proton.me>
2024-05-03 13:58:48 -0400

    Bunch of changes 1. Renamed some variables 2. Reorganized some functions 3. Reworked comments to show highlights with the Better Comments extension for VS Code 4. Reduced variables by inserting their values directly to where they needed to be. 4a. I know this isn't best practice, but it helped me reduce lines and since these values won't be changing in the app.py file itself I don't think it is a big deal.

--- app.py
+++ app.py
@@ -36,51 +36,42 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        # Set window default settings
+        # * Set window default settings
         self.setWindowTitle("Scramble Generator")
-        self.default_window_min_width = config["min_window_size"]["width"]
-        self.default_window_min_height = config["min_window_size"]["height"]
         self.setMinimumSize(
-            self.default_window_min_width, self.default_window_min_height
+            config["min_window_size"]["width"], config["min_window_size"]["height"]
         )
         self.setWindowIcon(QIcon(icon))
 
-        # Define normal variables
+        # *  Define normal variables
         self.is_running = False
         self.elapsed_time = QTime(0, 0)
-        self.default_theme = user_defaults_config["defaults"]["theme"]
-        self.default_num_moves = user_defaults_config["defaults"]["num_moves"]
-        self.default_puzzle_type = user_defaults_config["defaults"]["puzzle_type"]
-        self.default_min_num_moves = config["num_moves_range"]["min"]
-        self.default_max_num_moves = config["num_moves_range"]["max"]
-        self.default_num_moves_for_two_by_two = config["num_moves_per_puzzle_defaults"][
-            "2x2"
-        ]
-        self.default_num_moves_for_three_by_three = config[
-            "num_moves_per_puzzle_defaults"
-        ]["3x3"]
+
         self.puzzle_type_list = []
         for puzzle in config["puzzle_type_list"]:
             self.puzzle_type_list.append(puzzle)
+
         self.theme_list = []
         for theme in list(themes_config)[:-1]:
             self.theme_list.append(theme)
 
-        # Create end user widgets and apply settings to them
+        # * Create end user widgets and apply settings to them
         self.scramble_button = QPushButton("Generate Scramble")
 
         self.puzzle_type = QComboBox()
         self.puzzle_type.addItems(self.puzzle_type_list)
-        self.puzzle_type.setCurrentText(self.default_puzzle_type)
+        self.puzzle_type.setCurrentText(user_defaults_config["defaults"]["puzzle_type"])
 
         self.num_moves = QSpinBox()
-        self.num_moves.setRange(self.default_min_num_moves, self.default_max_num_moves)
-        self.num_moves.setValue(self.default_num_moves)
+        self.num_moves.setRange(
+            config["num_moves_range"]["min"], config["num_moves_range"]["max"]
+        )
+        self.num_moves.setValue(user_defaults_config["defaults"]["num_moves"])
         self.num_moves.lineEdit().setReadOnly(True)
 
         self.theme_picker = QComboBox()
         self.theme_picker.addItems(self.theme_list)
-        self.theme_picker.setCurrentText(self.default_theme)
+        self.theme_picker.setCurrentText(user_defaults_config["defaults"]["theme"])
 
         self.scramble = QLabel()
         self.scramble.setAlignment(
@@ -98,9 +89,9 @@ class MainWindow(QMainWindow):
         self.timer_output.setMaximumHeight(22)
 
         self.timer = QTimer()
-        self.timer.interval = 10  # Milliseconds
+        self.timer.interval = 10  # * Milliseconds
 
-        # Define button connections and/or actions
+        # * Define button connections and/or actions
         self.scramble_button.pressed.connect(self.get_moves)
         self.puzzle_type.currentTextChanged.connect(self.set_default_num_moves)
         self.theme_picker.currentIndexChanged.connect(self.toggle_theme)
@@ -108,12 +99,12 @@ class MainWindow(QMainWindow):
         self.timer_button.pressed.connect(self.toggle_timer)
         self.timer.timeout.connect(self.update_time)
 
-        # Create layouts
+        # * Create layouts
         self.page = QVBoxLayout()
         self.inputs = QHBoxLayout()
         self.timer_section = QHBoxLayout()
 
-        # Add widgets to layouts
+        # * Add widgets to layouts
         self.inputs.addWidget(self.scramble_button)
         self.inputs.addWidget(self.puzzle_type)
         self.inputs.addWidget(self.num_moves)
@@ -123,7 +114,7 @@ class MainWindow(QMainWindow):
         self.timer_section.addWidget(self.timer_button)
         self.timer_section.addWidget(self.timer_output)
 
-        # Setup overall page layout and set default window theme
+        # * Setup overall page layout and set default window theme
         self.page.addLayout(self.inputs)
         self.page.addWidget(self.scramble)
         self.page.addLayout(self.timer_section)
@@ -134,6 +125,14 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.gui)
 
         self.toggle_theme()
+
+    def toggle_theme(self):
+        self.theme = self.theme_picker.currentText()
+        self.set_theme()
+        self.apply_theme(self)
+        # * Apply theme to all child widgets
+        for widget in self.findChildren(QWidget):
+            self.apply_theme(widget)
 
     def set_theme(self):
         self.theme_stylesheet = """
@@ -153,28 +152,18 @@ class MainWindow(QMainWindow):
     def apply_theme(self, widget):
         widget.setStyleSheet(self.theme_stylesheet)
 
-    def toggle_theme(self):
-        self.theme = self.theme_picker.currentText()
-        self.set_theme()
-        self.apply_theme(self)
-        # Apply theme to all child widgets
-        for widget in self.findChildren(QWidget):
-            self.apply_theme(widget)
-
     def get_moves(self):
         scramble = ScrambleGenerator(self.puzzle_type.currentText())
         self.scramble.setText(scramble.generate_scramble(self.num_moves.value()))
 
     def set_default_num_moves(self):
-        match self.puzzle_type.currentText():
-            case "2x2":
-                self.num_moves.setValue(self.default_num_moves_for_two_by_two)
-            case "3x3":
-                self.num_moves.setValue(self.default_num_moves_for_three_by_three)
+        self.num_moves.setValue(
+            config["puzzle_default_moves"][self.puzzle_type.currentText()]
+        )
 
     def toggle_timer(self):
         if not self.is_running:
-            self.timer.start(100)  # Update display every 100 milliseconds
+            self.timer.start(100)  # * Update display every 100 milliseconds
             self.timer_button.setText("Stop Timer")
             self.is_running = True
         else:
@@ -186,7 +175,7 @@ class MainWindow(QMainWindow):
     def update_time(self):
         self.elapsed_time = self.elapsed_time.addMSecs(
             100
-        )  # Increment time by 100 milliseconds
+        )  # * Increment time by 100 milliseconds
         self.timer_output.setText(self.elapsed_time.toString("mm:ss.z"))
 
         if not self.is_running:
